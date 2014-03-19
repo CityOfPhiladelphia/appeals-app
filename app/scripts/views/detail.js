85c52b6a59a820fdaea7f4c6c8d8d8a15a2e3868
davewalk <daviddwalk@gmail.com>
2014-03-18 21:44:32 -0400

    Displaying message in decision history table if there is not history

--- app/scripts/views/detail.js
+++ app/scripts/views/detail.js
@@ -5,10 +5,11 @@ define([
     'underscore',
     'backbone',
     'text!templates/detail.html',
+    'text!templates/no-decision-history.html',
     '../models/current-appeal',
     '../collections/decisions',
     '../views/decision'
-], function ($, _, Backbone, Template, CurrentAppeal, DecisionsCollection, DecisionView) {
+], function ($, _, Backbone, Template, NoDecisionHistoryTemplate, CurrentAppeal, DecisionsCollection, DecisionView) {
     'use strict';
 
     var DetailView = Backbone.View.extend({
@@ -47,12 +48,19 @@ define([
         });
       },
 
+      displayNoHistoryMessage: function(collection, resp) {
+        if (resp.status === 404) {
+          $('.decision-history-table').append(_.template(NoDecisionHistoryTemplate));
+        }
+      },
+
       onRender: function() {
         var self = this;
         $('#decision-history').one('show.bs.collapse', function(e) {
           e.stopPropagation();
           self.collection = new DecisionsCollection({ appealNum: CurrentAppeal.get('appealNum') });
           self.collection.once('sync', self.addDecisions, self);
+          self.collection.once('error', self.displayNoHistoryMessage, self);
           self.collection.fetch({ appealNum: CurrentAppeal.get('appealNum') });
         });
       }
