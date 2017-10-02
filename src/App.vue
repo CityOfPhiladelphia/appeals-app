afa9ec69074e2eb246cd88c2087ae377c4973915
Alejandro Lopez <alejo0109@gmail.com>
2017-10-02 10:45:16 -0400

    IE Testing

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
           <div class="columns">
             <div class="awkward">
               <h3>Ok, this is awkward =(</h3>
-              <p>Hmmm, could you please upgrade your Internet Browser? Currently, you are using an unsupported and insecure browser. <strong>Some features are not available</strong></p>
+              <p>Hmmm, could you please upgrade your Internet Browser? You are using an unsupported and insecure browser. <strong>Some features may be disabled.</strong></p>
             </div>
           </div>
         </div>
