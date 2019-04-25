56d95983cbf7269145f02df1759b9a3cd0395de4
Karissa <k@karissademi.com>
2019-04-25 16:34:21 -0400

    Update with L+I info

--- static/app.config.js
+++ static/app.config.js
@@ -1,20 +1,21 @@
 const app_config = {
   types: {
-    RB_ZBA: {
-      id: 'RB_ZBA',
-      text: 'ZBA',
-      description: 'Zoning Board of Appeals',
+    // RB_ZBA: {
+    //   id: 'RB_ZBA',
+    //   text: 'ZBA',
+    //   description: 'Zoning Board of Appeals',
+    // },
+    RB_BBS: {
+      id: 'RB_BBS',
+      text: 'BBS',
+      description: 'Board of Building Standards'
     },
-    // RB_BBS: {
-    //   id: 'RB_BBS',
-    //   text: 'BBS',
-    //   description: 'Board of Building Standards'
-    // },
-    // RB_LIRB: {
-    //   id: 'RB_LIRB',
-    //   text: 'LIRB',
-    //   description: 'L&I Review Board of Appeals'
-    // },
+    //LIRB and BLIR are two names for the same thing. 
+    RB_LIRB: {
+      id: 'RB_LIRB',
+      text: 'BLIR',
+      description: 'Board of Licenses and Inspections Review'
+    },
   }
 }
 
